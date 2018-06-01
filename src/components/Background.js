6ae6a02fd3f0e27d3c87b21db9d108e570824ba7
viktoriyamyalo <viktoriya.myalo.lystopad@gmail.com>
2018-06-01 17:09:24 +0300

    Saved users to localStorage

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,25 +1,38 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import { STORAGE_KEY } from '../constants';
 
 class Background extends Component {
     state = { pictures: [] };
 
     componentWillMount() {
-        fetch('https://randomuser.me/api/?results=5000')
-            .then(result => result.json())
-            .then(resultJSON => {
-                let pictures = resultJSON.results.map((user) => {
-                    return (
-                        <div key={user.login.md5} className="background-picture">
-                            <img src={user.picture.large} alt="User" />
-                        </div>
-                    );
-                })
-                this.setState({ pictures: pictures });
-            });
+        if(localStorage.getItem(`${STORAGE_KEY}:users`)) {
+            fetch('https://randomuser.me/api/?results=5000')
+                .then(result => result.json())
+                .then(users => {
+                    localStorage.setItem(`${STORAGE_KEY}:users`, users);
+                    let pictures = users.results.map((user) => {
+                        return (
+                            <div key={user.login.md5} className="background-picture">
+                                <img src={user.picture.large} alt="User" />
+                            </div>
+                        );
+                    })
+                    this.setState({ pictures: pictures });;
+                });
+        } else {
+           let users = localStorage.getItem(`${STORAGE_KEY}:users`);
+           console.log(users);
+           let pictures = users.results.map((user) => {
+               return (
+                   <div key={user.login.md5} className="background-picture">
+                       <img src={user.picture.large} alt="User" />
+                   </div>
+               );
+           });
+           this.setState({ pictures: pictures});
+        }
 
-
-        console.log(this.state.pictures);
     }
     render() {
         return (
